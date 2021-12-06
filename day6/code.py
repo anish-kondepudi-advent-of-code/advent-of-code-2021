aa53bbf07d911102ff2c42c65c06fccfc500b8e5
Anish Kondepudi <72046642+anish-kondepudi@users.noreply.github.com>
2021-12-05 21:14:17 -0800

    day6

---
+++ day6/code.py
@@ -0,0 +1,21 @@
+def readfile(filename):
+    with open("input.txt") as file:
+        return list(map(int,file.readlines()[0].split(',')))
+
+def countFishes(filename,days):
+    startFishes = readfile(filename)
+
+    fishes = [0]*9
+    for fish in startFishes:
+        fishes[fish] += 1
+
+    for _ in range(days):
+        newFish = fishes.pop(0)
+        fishes.append(newFish)
+        fishes[6] += newFish
+
+    return sum(fishes)
+
+
+print(countFishes("input.txt",80))
+print(countFishes("input.txt",256))
